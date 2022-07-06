ceef8d980f6831b03383d8784aef0cf0b1b8f8fa
Tkacz <gabrielmt2@al.insper.edu.br>
2022-07-06 05:25:03 -0300

    att

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# nba-headshot-api
+![NBA Logo](https://logodownload.org/wp-content/uploads/2016/11/nba-logo-1.png)
+<br><br>
+
+# Welcome to nba-headshot-api: An open API for retrieving active NBA players' official headshots
---
+++ source_code/api.py
@@ -0,0 +1,38 @@
+import pandas as pd, ast
+from flask import Flask
+from flask_restful import Resource, Api, reqparse
+
+app = Flask(__name__)
+api = Api(app)
+
+class Players(Resource):
+    def get(self):
+        parser = reqparse.RequestParser()
+        
+        parser.add_argument('id', required = False)
+        parser.add_argument('player', required = False)
+        parser.add_argument('team', required = False)
+
+        args = parser.parse_args()
+        
+        data = pd.read_csv('data.csv')
+        data = data.to_dict()
+        return {'data': data}, 200
+    
+    def post(self):
+        return {'Message': 'Action not allowed.'}, 405
+    
+    def put(self):
+        return {'Message': 'Action not allowed.'}, 405
+    
+    def delete(self):
+        return {'Message': 'Action not allowed.'}, 405
+
+class Teams(Resource):
+    pass
+
+api.add_resource(Players, '/players')
+api.add_resource(Teams, '/teams')
+
+if __name__ == '__main__':
+    app.run(debug=True)
